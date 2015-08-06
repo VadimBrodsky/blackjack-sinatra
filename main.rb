3efa37ba82039a9d41487a4160a28a05c1d0ec3b
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-06 06:59:05 -0400

    Add additional routes, start on helpers

--- main.rb
+++ main.rb
@@ -1,14 +1,32 @@
 require 'rubygems'
 require 'sinatra'
 
-# set :sessions, true
 
+# set :sessions, true
 use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'the_secret_of_secrets'
 
+
+module PlayerHelpers
+  def logged_in?
+    !session[:player_name].nil?
+  end
+end
+
+helpers PlayerHelpers
+
 get '/' do
   erb :home
+end
+
+get '/game/new/?' do
+  'new game'
+  logged_in?.to_s
+end
+
+get '/player/new/?' do
+  'new player'
 end
 
 # Examples of Sinatra Responses
