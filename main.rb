9da56333726c2477d764ac9ab328c626ffea03fa
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-11 08:41:02 -0400

    Experiment with JSON serialization / deserialization

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
 
 # set :sessions, true
 use Rack::Session::Cookie, key: 'rack.session',
@@ -13,19 +14,28 @@ module PlayerHelpers
 end
 
 class Deck
+  attr_accessor :cards
+
   CARDS = { '2' => 2, '3' => 3, '4' => 4, '5' => 5,
           '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10,
-          'jack' => 10, 'queen' => 10, 'king' => 10, 'ace' => [1, 11] }
-  SUITS = { 'spades' => '♠', 'hearts' => '♥', 'diamonds' => '♦', 'clubs' => '♣' }
+          'j' => 10, 'q' => 10, 'k' => 10, 'a' => [1, 11] }
+  SUITS = { 's' => 'spades', 'h' => 'hearts', 'd' => 'diamonds', 'c' => 'clubs' }
 
-  def initialize
-    @deck = CARDS.keys.product(SUITS.keys).shuffle
+  def initialize(data=nil)
+    if data.nil?
+      self.cards = CARDS.keys.product(SUITS.keys).shuffle
+    else
+      self.cards = data
+    end
   end
 
   def to_s
-    'I am a string'
+    cards.to_s
   end
 
+  def to_json
+    cards.to_json
+  end
 end
 
 helpers PlayerHelpers
