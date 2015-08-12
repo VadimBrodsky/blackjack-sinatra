1704280655a43d97eb316c6f4312cb0af5b4d6ee
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-12 07:00:38 -0400

    Remove classes to external files

---
+++ lib/deck.rb
@@ -0,0 +1,24 @@
+class Deck
+  attr_accessor :cards
+
+  CARDS = { '2' => 2, '3' => 3, '4' => 4, '5' => 5,
+          '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10,
+          'j' => 10, 'q' => 10, 'k' => 10, 'a' => [1, 11] }
+  SUITS = { 's' => 'spades', 'h' => 'hearts', 'd' => 'diamonds', 'c' => 'clubs' }
+
+  def initialize(data=nil)
+    if data.nil?
+      self.cards = CARDS.keys.product(SUITS.keys).shuffle
+    else
+      self.cards = data
+    end
+  end
+
+  def to_s
+    cards.to_s
+  end
+
+  def to_json
+    cards.to_json
+  end
+end
--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
+require_relative 'lib/deck'
+
 # set :sessions, true
 use Rack::Session::Cookie, key: 'rack.session',
                            path: '/',
@@ -13,30 +15,6 @@ module PlayerHelpers
   end
 end
 
-class Deck
-  attr_accessor :cards
-
-  CARDS = { '2' => 2, '3' => 3, '4' => 4, '5' => 5,
-          '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10,
-          'j' => 10, 'q' => 10, 'k' => 10, 'a' => [1, 11] }
-  SUITS = { 's' => 'spades', 'h' => 'hearts', 'd' => 'diamonds', 'c' => 'clubs' }
-
-  def initialize(data=nil)
-    if data.nil?
-      self.cards = CARDS.keys.product(SUITS.keys).shuffle
-    else
-      self.cards = data
-    end
-  end
-
-  def to_s
-    cards.to_s
-  end
-
-  def to_json
-    cards.to_json
-  end
-end
 
 helpers PlayerHelpers
 
@@ -48,10 +26,15 @@ get '/game/new/?' do
   if player_known?
     @player_name = session[:player_name]
     @deck = Deck.new
+    session[:deck] = @deck.to_json
     erb :'game/new'
   else
     redirect '/player/new'
   end
+end
+
+get '/game/?' do
+
 end
 
 get '/player/?' do
