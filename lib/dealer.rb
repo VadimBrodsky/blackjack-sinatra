207331205b7813f0f4859c1035895864f2ac1c66
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-16 12:21:56 -0400

    Add dealer hide card funcitonality

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -3,10 +3,12 @@ require 'json'
 
 class Dealer
   attr_accessor :name, :cards
+  attr_reader :hide_card
 
   def initialize
     self.name = 'Dealer'
     self.cards = []
+    @hide_card = true
   end
 
   def hand=(card)
@@ -15,17 +17,26 @@ class Dealer
   end
 
   def hand
-    @cards
+    if hide_card?
+      @cards.map.with_index { |c,i| i == 0 ? nil : c }
+    else
+      @cards
+    end
+  end
+
+  def hide_card?
+    @hide_card
   end
 
   def save_to_session
-    {name: name, hand: cards}.to_json
+    {name: name, hand: cards, hide: @hide_card}.to_json
   end
 
   def load_from_session(session_json)
     data = JSON.parse(session_json)
     self.name = data['name']
     self.hand = data['hand']
+    @hide_card = data['hide']
     self
   end
 end
