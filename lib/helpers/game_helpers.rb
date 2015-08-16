3864d51f45fd45c106330786da8bce144d70160e
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-16 14:11:46 -0400

    Start on image helpers

--- lib/helpers/game_helpers.rb
+++ lib/helpers/game_helpers.rb
@@ -16,4 +16,34 @@ module GameHelpers
     save_dealer
     save_deck
   end
+
+  def image_name(card)
+    if card.nil?
+      'cover.jpg'
+    else
+      "#{suit_name(card)}_#{face_name(card)}.jpg"
+    end
+  end
+
+  def face_name(card)
+    face = card[0].to_i
+    if face == 0
+      face = case face
+        when 'j' then 'jack'
+        when 'q' then 'queen'
+        when 'k' then 'king'
+        when 'a' then 'ace'
+      end
+    end
+    face
+  end
+
+  def case_name(card)
+    case card[1]
+      when 'c' then 'clubs'
+      when 'd' then 'diamonds'
+      when 'h' then 'hearts'
+      when 's' then 'spaces'
+    end
+  end
 end
